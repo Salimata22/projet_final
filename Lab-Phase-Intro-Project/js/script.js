3ae00e663afee64f191319589e8f532fe575b8d8
Salimata22 <togo27317@gmail.com>
2023-05-09 21:32:29 +0000

    enCours

--- Lab-Phase-Intro-Project/js/script.js
+++ Lab-Phase-Intro-Project/js/script.js
@@ -1,1 +1,12 @@
-// all begin here
+
+function champ() {
+    style.backgroundColor = rgba(19, 32, 220, 0.8) ;
+    
+}
+
+let models = document.querySelectorAll('.one-one');
+models.addEventListener('mouseover',champ);
+
+
+
+   
